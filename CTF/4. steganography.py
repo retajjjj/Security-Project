df9aa1f3972c910d540b2798b185201d62ba8f6a
retajjjj <retagmohamed2004@gmail.com>
2026-04-28 11:18:58 +0300

    folder-structure

---
+++ CTF/4. steganography.py
@@ -0,0 +1,29 @@
+from PIL import Image
+
+image = Image.open("../CTF_DATA/CTF4/stego.png")
+w, h = image.size
+px = image.load()
+px[0,8]
+
+
+def int2bin(n):
+    return "{0:08b}".format(n)
+
+
+def readLSB():
+    binaryLSB = ""
+    for y in range(h):
+        for x in range(w):
+            binaryLSB += int2bin(px[x,y])[7]
+
+    output = ""
+    for i in range(int(len(binaryLSB) / 8)):
+        output += chr(int(binaryLSB[i * 8:i * 8 + 8], 2))
+
+    return output
+
+def main():
+    op = readLSB()
+
+
+    
